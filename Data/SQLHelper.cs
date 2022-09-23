e99928733ff6559281648008f317a968bb0e31c0
daniel3scripts <paul.lugo@tecsup.edu.pe>
2022-09-23 00:00:10 -0500

    comit1

---
+++ Data/SQLHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+namespace Data
+{
+    static class SQLHelper
+    {
+        public static string Connection { get; set; } = "Data Source=HUGO-PC\\MSSQLSERVER2014;" +
+                                                   "Initial Catalog=Neptuno;Integrated Security=true";
+        /// <summary> 
+        /// Set the connection, command, and then execute the command with non query. 
+        /// </summary> 
+        public static Int32 ExecuteNonQuery(String connectionString, String commandText,
+            CommandType commandType, params SqlParameter[] parameters)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(commandText, conn))
+                {
+                    // There're three command types: StoredProcedure, Text, TableDirect. The TableDirect  
+                    // type is only for OLE DB.   
+                    cmd.CommandType = commandType;
+                    cmd.Parameters.AddRange(parameters);
+
+                    conn.Open();
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static SqlDataReader ExecuteReader(String connectionString, String
+            commandText,CommandType commandType,params SqlParameter[] parameters)
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+            using (SqlCommand cmd = new SqlCommand(commandText, conn))
+            {
+                cmd.CommandType= commandType;
+                cmd.Parameters.AddRange (parameters);
+                conn.Open();
+
+                SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                return reader;
+            }
+        }
+
+    }
+}
